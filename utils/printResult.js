25e322f577ee31affaa078dc47ae59762bec2090
Afsar <afsar@entrar.in>
2021-03-29 17:58:34 +0530

    initial commit

--- utils/printResult.js
+++ utils/printResult.js
@@ -1,7 +1,7 @@
 const printResult = result => {
 	result.forEach(file => {
 		process.stdout.write(file.name)
-		file.isDeleted && process.stdout.write(' [deleted]'.red.bold)
+		file.isDeleted && process.stdout.write(' [removed]'.red.bold)
 		file.isModified && process.stdout.write(' [modified]'.yellow.bold)
 		file.isAdded && process.stdout.write(' [added]'.green.bold)
 		file.isModified && process.stdout.write('\n' + file.changes)
